9f7278e74472bdcaa0320e99a075e66ef1118f9a
GOLF <golf.prptmk@gmail.com>
2020-06-29 23:00:07 +0700

    app initialized

---
+++ dist/precache-manifest.180e2693768b9161df79d47fe16dc157.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58e3148b073cc4a7c36c",
+    "url": "/css/app.6f5ce249.css"
+  },
+  {
+    "revision": "4def70fae7a1d902b765e59367de606a",
+    "url": "/img/mockup/profile.png"
+  },
+  {
+    "revision": "cc4cb758a6fd757f92389653bb6e4bdf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4561d0aa34e433fca594",
+    "url": "/js/about.26752d07.js"
+  },
+  {
+    "revision": "58e3148b073cc4a7c36c",
+    "url": "/js/app.a390f802.js"
+  },
+  {
+    "revision": "7ffda3d2ab24a3e0c7a0",
+    "url": "/js/chunk-vendors.22901a14.js"
+  },
+  {
+    "revision": "81b608c15c1b3556695685529d20c0aa",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
