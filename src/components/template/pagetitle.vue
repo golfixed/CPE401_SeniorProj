1f15566e46e9e1f20bbdfb89b74a1c8ee803de51
Peerapong Thammakaew <44258945+golfixed@users.noreply.github.com>
2020-11-11 21:56:51 +0700

    button layout added

--- src/components/template/pagetitle.vue
+++ src/components/template/pagetitle.vue
@@ -8,8 +8,18 @@
     </div>
 
     <div class="titlebar-right">
-      <div class="option-btn">
-        <img src="/img/icons/threedots_white.svg" draggable="false" />
+      <div class="top-btn" v-if="currentRoute == '/'">
+        <i class="fas fa-bell"></i>
+        <div class="noti-badge"></div>
+      </div>
+      <div
+        class="top-btn"
+        v-if="currentRoute == '/classrooms' || currentRoute == '/chats'"
+      >
+        <i class="fas fa-plus"></i>
+      </div>
+      <div class="top-btn">
+        <i class="fas fa-bars"></i>
       </div>
     </div>
   </div>
@@ -18,19 +28,23 @@
 <script>
 export default {
   name: "pagetitle",
+  computed: {
+    currentRoute: function () {
+      return this.$route.path;
+    },
+  },
 };
 </script>
 
 <style lang="scss" scpoed>
 #app-title {
-  height: 60px;
-  padding: 0 30px;
+  height: 80px;
   display: flex;
   justify-content: space-between;
   align-items: center;
   background: rgba(255, 255, 255, 0.8);
   backdrop-filter: blur(80px);
-  padding: 5px 20px 20px 20px;
+  padding: 0 20px;
   @media screen and (max-width: 375px) {
     padding: 0 20px;
   }
@@ -67,16 +81,39 @@ export default {
     }
   }
   .titlebar-right {
+    display: flex;
+    align-items: center;
     justify-content: flex-end;
-    .option-btn {
-      width: 20px;
-      height: 30px;
+    .top-btn {
+      width: 25px;
+      height: 50px;
+      margin-right: 20px;
       display: flex;
-      justify-content: flex-end;
       align-items: center;
-      img {
-        width: 20px;
+      justify-content: center;
+      position: relative;
+      i {
+        font-size: 24px;
+        background: linear-gradient(
+          135deg,
+          rgba(60, 102, 151, 1) 0%,
+          rgba(59, 155, 80, 1) 100%
+        );
+        -webkit-background-clip: text;
+        -webkit-text-fill-color: transparent;
       }
+      div.noti-badge {
+        height: 10px;
+        width: 10px;
+        background-color: #df5d5d;
+        border-radius: 100px;
+        position: absolute;
+        top: 10px;
+        right: -5px;
+      }
+    }
+    .top-btn:last-child {
+      margin-right: 0;
     }
   }
 }
