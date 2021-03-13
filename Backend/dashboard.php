159560936fd01e1ca0f96228a0abbe260260d314
nithimy <57937879+nithimy@users.noreply.github.com>
2021-03-13 14:48:31 +0700

    Dashboard

--- Backend/dashboard.php
+++ Backend/dashboard.php
@@ -1,20 +1,28 @@
 <?php
-require('class.php');
-include("auth.php");
+include('server.php');
+session_start();
+if(!isset($_SESSION["username"])){
+header("Location: login.php");
+exit(); }
 ?>
 <!DOCTYPE html>
 <html>
+
 <head>
-<meta charset="utf-8">
-<title>Dashboard - Secured Page</title>
-<link rel="stylesheet" href="css/style.css" />
+
+    <meta charset="UTF-8">
+    <meta name="dashboard" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+    <link rel="stylesheet" href="css/style.css"/>
 </head>
+
 <body>
-<div class="form">
-<p>Dashboard</p>
-<p>This is another secured page.</p>
-<p><a href="index.php">Home</a></p>
-<a href="logout.php">Logout</a>
-</div>
+    <div class="form">
+        <p>Dashboard</p>
+        <p>This is another secured page.</p>
+        <p><a href="index.php">Home</a></p>
+        <a href="logout.php">Logout</a>
+    </div>
 </body>
+
 </html>
