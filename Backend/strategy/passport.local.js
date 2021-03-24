ee9aa21bdd064f77ae6ce269863e0af8c883dcaf
yee2542 <yee2542gtl@gmail.com>
2021-03-24 18:20:16 +0700

    fix bad passport strategy

--- Backend/strategy/passport.local.js
+++ Backend/strategy/passport.local.js
@@ -1,9 +1,21 @@
 const passport = require("passport"),
   LocalStrategy = require("passport-local").Strategy;
 
-passport.use(
-  new LocalStrategy(function (username, password, done) {
-    if (username === "hello") return done(null, { username: "hello" });
-    else return done(null, false, { message: "invalid user" });
-  })
-);
+module.exports = () => {
+  passport.serializeUser(function (user, done) {
+    done(null, user.username);
+  });
+
+  passport.deserializeUser(function (user, done) {
+    done(null, user);
+  });
+
+  passport.use(
+    new LocalStrategy(function (username, password, done) {
+      // user authen check
+      if (username === "hello")
+        return done(null, { username: "hello", createAt: new Date() });
+      else return done(null, false, { message: "invalid user" });
+    })
+  );
+};
