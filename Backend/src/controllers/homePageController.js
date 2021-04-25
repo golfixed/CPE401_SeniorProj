5c743c03aba23909a89620f89d1258519ffc1984
volktgod <volktgod@gmail.com>
2021-04-25 17:32:58 +0700

    temp path to homepage
    temp path to homepage after login successfully

--- Backend/src/controllers/homePageController.js
+++ Backend/src/controllers/homePageController.js
@@ -1,8 +1,9 @@
 //GET OUR HOMEPAGE
 let handleHelloWorld = async (req, res) => {
-    return res.render("homepage.ejs",{
-        user: req.user
-    });
+    // return res.render("homepage.ejs",{
+    //     user: req.user
+    // });
+    return res.send({ error: false, message: "Home Page"});
 };
 
 module.exports = {
