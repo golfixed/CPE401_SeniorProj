98d5e8f1fa3f3db3a546afabef4eac8bba588d16
volktgod <volktgod@gmail.com>
2021-04-27 20:34:01 +0700

    Edit path /login

--- Backend/src/routes/web.js
+++ Backend/src/routes/web.js
@@ -10,16 +10,31 @@ import initPassportLocal from "../controllers/passportLocalController";
 initPassportLocal();
 
 let router = express.Router();
+const jwt = require('jsonwebtoken')
 
 let initWebRoutes = (app) => {
     router.get("/", loginController.checkLoggedIn, homePageController.handleHelloWorld);
     router.get("/login",loginController.checkLoggedOut, loginController.getPageLogin);
-    router.post("/login", passport.authenticate("local", {
-        successRedirect: "/",
-        failureRedirect: "/login",
-        successFlash: true,
-        failureFlash: true
-    }));
+    //in case of Redirect to Homepage
+
+    // router.post("/login", passport.authenticate("local", {
+    //     successRedirect: "/",
+    //     failureRedirect: "/login",
+    //     successFlash: true,
+    //     failureFlash: true
+    // }));
+    router.post('/login', (req, res, next) => {
+  
+        passport.authenticate('local', {session: false}, (err, user, info) => {
+            if (err) return next(err)
+            if(user) {
+                const token = jwt.sign(user, 'your_jwt_secret')
+                return res.json({user, token})
+            } else {
+                return res.status(422).json(info)
+             }
+        })(req, res, next);
+    });
 
     router.get("/register", registerController.getPageRegister);
     router.post("/register", registerController.createNewUser);
