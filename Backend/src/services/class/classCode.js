7f5e8793b88a6d41a19ca2333cd636344f8caedc
volktgod <volktgod@gmail.com>
2021-05-07 02:25:08 +0700

    del passport(not use)

--- Backend/src/services/class/classCode.js
+++ Backend/src/services/class/classCode.js
@@ -1,13 +1,12 @@
 import dbCon from "../../configs/DBConnection";
 import express from "express";
-import passport from "passport";
+//check exist class
 
 let classCode = express();
 
 classCode.get('/:class_code', (req, res) =>{
     let class_code = req.params.class_code;
     const {section} = req.query;
-    
     if(!class_code){
         return res.status(400).send({ error: true, message: "Please provide class code"});
     }else
