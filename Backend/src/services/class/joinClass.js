2632355707ab5d2e359606559a62a853884d26d2
volktgod <volktgod@gmail.com>
2021-05-20 04:08:13 +0700

    add join class

--- Backend/src/services/class/joinClass.js
+++ Backend/src/services/class/joinClass.js
@@ -6,17 +6,15 @@ let joinClass = express();
 joinClass.post('/joinClass', (req, res) =>{
     let join_code = req.body.join_code;
     let class_id = req.body.class_id;
-    let account_id = req.body.id;
-    let user = {
-        id: req.body.id,
-    }
+    let account_id = req.body.account_id;
 
         if(!join_code){
             res.status(400).send({error: true, message: "Please provide join code to enter class"})
         }else{
-            let sql= "SELECT * FROM fromaccount LEFT JOIN fromclass ON fromaccount.id = fromclass.id UNION ALL SELECT * FROM fromaccount RIGHT JOIN fromclass ON fromaccount.id = fromclass.id";
+            // let sql= "INSERT INTO class_member (class, role_member, account) SELECT * FROM fromaccount LEFT JOIN fromclass ON fromaccount.id = fromclass.id UNION ALL SELECT * FROM fromaccount RIGHT JOIN fromclass ON fromaccount.id = fromclass.id";
+            // let sql= "INSERT INTO class_member (class, role_member, account) VALUES (?, ?, ?)";
             // dbCon.query("INSERT INTO class_member (class) SELECT id FROM class WHERE join_code = ?", [join_code], (error, results, fields) =>{
-            dbCon.query(sql, (error, results, fields) =>{
+            dbCon.query('INSERT INTO class_member (account, role_member, class) SELECT account.id, account.role, class.id FROM account, class WHERE class.id =? AND account.id = ?',[class_id,account_id], (error, results, fields) =>{
             // dbCon.query("INSERT INTO class_member [(account, role_member)] SELECT id, role FROM account WHERE email = 'volktgod@gmail.com'",[account_id, join_code], (error, results, fields) =>{
                 if (error) throw error;
         
