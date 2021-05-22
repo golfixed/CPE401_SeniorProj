05e55c5a8c279215cc6dd49b8fb8ce2b89afbafe
volktgod <volktgod@gmail.com>
2021-05-22 18:51:19 +0700

    change create poll dir

--- Backend/src/services/class/poll/createPoll.js
+++ Backend/src/services/class/poll/createPoll.js
@@ -8,28 +8,25 @@ createPoll.post('/createPoll', (req, res) => {
         published: req.body.published,
         expired: req.body.expired,
         content: req.body.content,
-        option1: req.body.option1,
-        option2: req.body.option2,
-        option3: req.body.option3,
-        option4: req.body.option4,
-        option5: req.body.option5,
+        options: req.body.options,
         create_by: req.body.create_by,
         update_by: req.body.update_by
     }
     
-    if(!poll.option1 || !poll.option2){
+    if(!poll.options){
         res.status(400).send({error: true, message: "Please provide at least 2 options"})
-    }
-    dbCon.query('INSERT INTO poll SET ?', poll, (error, results, fields) =>{
-        if (error) throw error;
-
-        return res.status(200).send({
-            error: false,
-            data: results,
-            poll: poll,
-            message: "create poll successfully"
+    }else{
+        dbCon.query('INSERT INTO poll SET ?', poll, (error, results, fields) =>{
+            if (error) throw error;
+    
+            return res.status(200).send({
+                error: false,
+                data: results,
+                poll: poll,
+                message: "create poll successfully"
+            })
         })
-    })
+    }
 })
 
 module.exports = createPoll;
