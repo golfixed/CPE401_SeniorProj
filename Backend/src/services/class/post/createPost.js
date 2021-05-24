b69b788fc674b0f6c16f12ec9de78f8316a1c9ca
volktgod <volktgod@gmail.com>
2021-05-24 19:29:38 +0700

    edit val ann and cc and path

--- Backend/src/services/class/post/createPost.js
+++ Backend/src/services/class/post/createPost.js
@@ -1,26 +1,23 @@
 import dbCon from "../../../configs/DBConnection";
-import classService from "../../../services/class/classService";
 import express from "express";
 
 let createPost = express();
 
-createPost.post("/:class/createPost", (req, res) => {
+createPost.post("/classrooms/:class/createPost", (req, res) => {
     
     let post = {
         class: req.params.class,
-        comment: req.body.comment,
-        announce: req.body.announce,
-        click_count:req.body.click_count, 
-        pic_url: req.body.pic_url,
         content: req.body.content,
+        comment: req.body.comment,
+        pic_url: req.body.pic_url,
         create_by: req.body.create_by,
-        update_by: req.body.update_by
+        update_by: req.body.update_by,
+        click_count:0, 
+        announce: false
     }
 
     if(!post.class){
         res.status(400).send({error: true, message: "Please provide class id"})
-    // }else if(!classService.checkExistClassById(post.class)){
-    //     res.status(400).send({error: true, message: `No class id ${post.class} exist`})
     }else{
         dbCon.query("INSERT INTO post SET ?", post, (error, results, fields) =>{
             if (error) throw error;
