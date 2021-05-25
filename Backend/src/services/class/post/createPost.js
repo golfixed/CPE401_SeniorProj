1a86bc84a49e07f5035ff2a928cdd71814bed380
volktgod <volktgod@gmail.com>
2021-05-25 12:44:22 +0700

    check class create post

--- Backend/src/services/class/post/createPost.js
+++ Backend/src/services/class/post/createPost.js
@@ -8,26 +8,38 @@ createPost.post("/classrooms/:class/createPost", (req, res) => {
     let post = {
         class: req.params.class,
         content: req.body.content,
-        comment: req.body.comment,
         pic_url: req.body.pic_url,
+        //create_by = account id
         create_by: req.body.create_by,
         update_by: req.body.update_by,
         click_count:0, 
         announce: false
     }
 
+    //Request Class id 
     if(!post.class){
         res.status(200).send({error: true, message: "Please provide class id"})
     }else{
-        dbCon.query("INSERT INTO post SET ?", post, (error, results, fields) =>{
-            if (error) throw error;
-    
-            return res.status(200).send({
-              error: false,
-              data: results,
-              post: post,
-              message: "Post successfully"
-            })
+        //Check if class id exist 
+        dbCon.query("SELECT id FROM class WHERE id = ?",[post.class], (error, results) =>{
+            if(error) {console.log(error)}
+            else{
+                console.log(results)
+                if(results.length > 0){
+                    dbCon.query("INSERT INTO post SET ?", post, (error, results, fields) =>{
+                        if (error) throw error;
+                        
+                        return res.status(200).send({
+                            error: false,
+                            data: results,
+                            post: post,
+                            message: "Post successfully"
+                        })
+                    })
+                }else{
+                    res.status(200).send({error: true, message: "No class id in DB"})
+                }
+            }
         })
     }
 })
