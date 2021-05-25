6752fdcc7dc32bdd9004e1d3622ff20c33d1fbfd
volktgod <volktgod@gmail.com>
2021-05-26 03:33:14 +0700

    Update server.js

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -56,24 +56,24 @@ const storage = multer.diskStorage({
 const upload = multer({
     storage: storage,
     limits: {
-        fileSize: 1024*1024*10
+        fileSize: 1024 * 1024 * 10
     }
 })
 app.use('/profile', express.static('upload/images'));
 app.post("/uploadprofile", upload.single('image'), (req, res) => {
     let id = req.body.id;
-    let image  = `http://localhost:3000/profile/${req.file.filename}`;
+    let image = `http://localhost:3000/profile/${req.file.filename}`;
 
-    if(!id){
-        res.status(400).send({error: true, message: 'Please give your id to upload your profile picture'});
-    }else{
+    if (!id) {
+        res.status(400).send({ error: true, message: 'Please give your id to upload your profile picture' });
+    } else {
         dbCon.query("UPDATE account SET image = ? WHERE id = ?", [image, id], (error, results, fields) => {
             if (error) throw error;
-    
+
             res.status(200).send({
                 error: false,
                 account_id: id,
-                profile_url: image 
+                profile_url: image
             })
         })
         console.log(image);
@@ -83,18 +83,18 @@ app.post("/uploadprofile", upload.single('image'), (req, res) => {
 app.use('/classpic', express.static('upload/images'));
 app.post("/uploadclassprofile", upload.single('image'), (req, res) => {
     let id = req.body.id;
-    let image  = `http://localhost:3000/classpic/${req.file.filename}`;
+    let image = `http://localhost:3000/classpic/${req.file.filename}`;
 
-    if(!id){
-        res.status(400).send({error: true, message: 'Please provide class id to upload your profile picture'});
-    }else{
+    if (!id) {
+        res.status(400).send({ error: true, message: 'Please provide class id to upload your profile picture' });
+    } else {
         dbCon.query("UPDATE class SET class_pic = ? WHERE id = ?", [image, id], (error, results, fields) => {
             if (error) throw error;
-    
+
             res.status(200).send({
                 error: false,
                 class_id: id,
-                profile_url: image 
+                profile_url: image
             })
         })
         console.log(image);
@@ -104,18 +104,18 @@ app.post("/uploadclassprofile", upload.single('image'), (req, res) => {
 app.use('/postpic', express.static('upload/images'));
 app.post("/uploadpostpic", upload.single('image'), (req, res) => {
     let id = req.body.id;
-    let image  = `http://localhost:3000/postpic/${req.file.filename}`;
+    let image = `http://localhost:3000/postpic/${req.file.filename}`;
 
-    if(!id){
-        res.status(400).send({error: true, message: 'Please give POST ID to upload your picture'});
-    }else{
+    if (!id) {
+        res.status(400).send({ error: true, message: 'Please give POST ID to upload your picture' });
+    } else {
         dbCon.query("UPDATE post SET pic_url = ? WHERE id = ?", [image, id], (error, results, fields) => {
             if (error) throw error;
-    
+
             res.status(200).send({
                 error: false,
                 post_id: id,
-                profile_url: image 
+                profile_url: image
             })
         })
         console.log(image);
@@ -125,18 +125,18 @@ app.post("/uploadpostpic", upload.single('image'), (req, res) => {
 app.use('/commentpic', express.static('upload/images'));
 app.post("/uploadcommentpic", upload.single('image'), (req, res) => {
     let id = req.body.id;
-    let image  = `http://localhost:3000/commentpic/${req.file.filename}`;
+    let image = `http://localhost:3000/commentpic/${req.file.filename}`;
 
-    if(!id){
-        res.status(400).send({error: true, message: 'Please give COMMENT ID to upload your picture'});
-    }else{
+    if (!id) {
+        res.status(400).send({ error: true, message: 'Please give COMMENT ID to upload your picture' });
+    } else {
         dbCon.query("UPDATE comment SET pic_url = ? WHERE id = ?", [image, id], (error, results, fields) => {
             if (error) throw error;
-    
+
             res.status(200).send({
                 error: false,
                 comment_id: id,
-                profile_url: image 
+                profile_url: image
             })
         })
         console.log(image);
@@ -157,4 +157,5 @@ app.use(errHandler);
 initWebRoutes(app);
 
 let port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Building a login system with NodeJS is running on port ${port}!`));
+app.listen(3000, () => console.log(`Building a login system with NodeJS is running on port ${port}!`));
+// app.listen(3000, '192.168.2.34', () => console.log(`Building a login system with NodeJS is running on port ${port}!`));
