83d26b31bdb1552537c6cc24cb4530db84aae1c1
volktgod <volktgod@gmail.com>
2021-05-25 22:13:26 +0700

    Update delPost.js

--- Backend/src/services/class/post/delPost.js
+++ Backend/src/services/class/post/delPost.js
@@ -8,20 +8,32 @@ delPost.delete('/deletePost/:id', (req, res) => {
     let id = req.params.id;
 
     if (!id) {
-        return res.status(200).send({ error: true, message: "Please specify class id or account id"});
+        res.status(200).send({ error: true, message: "Please provide post id" })
     } else {
-        dbCon.query('DELETE FROM post WHERE id = ?', id, (error, results, fields) => {
-            if (error) throw error;
+        dbCon.query('SELECT id FROM post WHERE id = ?', [id], (error, results) => {
+            if (error) { console.log(error) }
+            else {
+                if (results.length > 0) {
+                    let post_id = results[0].id;
 
-            let message = "";
-            if (results.affectedRows === 0) {
-                message = "Post not found";
-            } else {
-                message = "Post successfully deleted";
+                    dbCon.query("DELETE FROM post WHERE id = ?", post_id, (error, results, fields) => {
+                        if (error) throw error;
+
+                        let message = "";
+                        if (results === undefined || results.length == 0) {
+                            message = `There's no post id = ${id} `;
+                        } else {
+                            message = `Delete post id = ${id} successfully`;
+                        }
+                        return res.status(200).send({ error: false, data: results[0], message: message })
+                    })
+                } else {
+                    res.status(200).send({ error: true, message: "No class id in DB" })
+                }
             }
-            return res.send({ error: false, data: results, message: message })
         })
     }
 })
+
 
 module.exports = delPost;
