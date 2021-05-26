e9d4ba3cefa525efa9402bf0b2f4c81f5e22991b
volktgod <volktgod@gmail.com>
2021-05-26 19:46:54 +0700

    Update vote.js

--- Backend/src/services/class/poll/vote.js
+++ Backend/src/services/class/poll/vote.js
@@ -3,24 +3,25 @@ import express from "express";
 
 let clickVotes = express();
 
-clickVotes.post('/clickVotes', (req, res) => {
+clickVotes.post('/clickvotes', (req, res) => {
     //options id
     let id = req.body.id;
     let options = req.body.options;
     
     if(!id){
         res.status(200).send({error: true, message: "Please provide at least 2 options"})
-    }
-    dbCon.query('UPDATE poll SET options = ? WHERE published = 1 AND id = ?', [options, id], (error, results, fields) =>{
-        if (error) throw error;
-
-        return res.status(200).send({
-            error: false,
-            data: results,
-            options: options,
-            message: "vote successfully"
+    }else{
+        dbCon.query('UPDATE poll SET options = ? WHERE published = 1 AND id = ?', [options, id], (error, results, fields) =>{
+            if (error) throw error;
+    
+            return res.status(200).send({
+                error: false,
+                data: results,
+                options: options,
+                message: "vote successfully"
+            })
         })
-    })
+    }
 })
 
 module.exports = clickVotes;
