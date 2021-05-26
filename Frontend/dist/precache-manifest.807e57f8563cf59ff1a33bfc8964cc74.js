0f2bf13c9f43cc27aabf1c2c59b4381745471469
Peerapong Thammakaew <44258945+golfixed@users.noreply.github.com>
2021-05-26 17:50:10 +0700

    LATEST VER

---
+++ Frontend/dist/precache-manifest.807e57f8563cf59ff1a33bfc8964cc74.js
@@ -0,0 +1,166 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "916f39a916ce1152c77d",
+    "url": "/css/Announcement.4042c126.css"
+  },
+  {
+    "revision": "20ec40598fbd9216e140",
+    "url": "/css/Chats.56aa6fe2.css"
+  },
+  {
+    "revision": "254d03c8fa4ae856057e",
+    "url": "/css/Login.c37ff94f.css"
+  },
+  {
+    "revision": "ebcdd4b9395afc266a8e",
+    "url": "/css/Regis.d21ca7d1.css"
+  },
+  {
+    "revision": "3abe3b5aa79b4d15e8df",
+    "url": "/css/Search.2e63f224.css"
+  },
+  {
+    "revision": "8daa152d5491ffdd7381",
+    "url": "/css/Settings.5a6322ad.css"
+  },
+  {
+    "revision": "dec554aec515c0b6d026",
+    "url": "/css/Welcome.e11cd808.css"
+  },
+  {
+    "revision": "e7e54dbd5070b1567613",
+    "url": "/css/app.19fcf394.css"
+  },
+  {
+    "revision": "e93dab2da991065ca6f8",
+    "url": "/css/chunk-vendors.d9764759.css"
+  },
+  {
+    "revision": "2177ef25e05f312a9b16225957da2240",
+    "url": "/img/btn/chat/dot_red.svg"
+  },
+  {
+    "revision": "1fd9d28f47ecb3b38f66e9f9433ba0f8",
+    "url": "/img/btn/chat/plus_circle.svg"
+  },
+  {
+    "revision": "ff1a9d8fcc5ac7e72c866b572a10fbed",
+    "url": "/img/btn/chevron.png"
+  },
+  {
+    "revision": "e26c4cf4c65877f3de8aa4d19fccb9af",
+    "url": "/img/btn/edit.svg"
+  },
+  {
+    "revision": "3b653fc49e5f649db2795fb32011ac0f",
+    "url": "/img/btn/email.png"
+  },
+  {
+    "revision": "b5e5e87d168ca517aa03fb6efe4338d0",
+    "url": "/img/btn/facebook.png"
+  },
+  {
+    "revision": "bb268ce0609ef62f83db9e6c15dfd2f5",
+    "url": "/img/btn/google.png"
+  },
+  {
+    "revision": "2b941b92519c07b45cd165aa9ee2757b",
+    "url": "/img/btn/item-chevron.svg"
+  },
+  {
+    "revision": "271f166d72f7bec40eb09dc138d853fa",
+    "url": "/img/btn/menuOption/clear.svg"
+  },
+  {
+    "revision": "aaad6c75f9aa815d50e3972a9dfb379e",
+    "url": "/img/btn/menuOption/noti.svg"
+  },
+  {
+    "revision": "5699fee28f27b984dc6a3e33bbb2270e",
+    "url": "/img/btn/menuOption/setting.svg"
+  },
+  {
+    "revision": "57bccd4a94d65c629abae71d207783b7",
+    "url": "/img/btn/menuOption/sort.svg"
+  },
+  {
+    "revision": "cad35465a4115e659593dfdd86ed1265",
+    "url": "/img/btn/plus.svg"
+  },
+  {
+    "revision": "7dd0c7ff0db6b4956f15beb57a4f31dd",
+    "url": "/img/logo.png"
+  },
+  {
+    "revision": "517a988145d5e8e0dcce6bce77e6f77a",
+    "url": "/img/mockup/class.png"
+  },
+  {
+    "revision": "4def70fae7a1d902b765e59367de606a",
+    "url": "/img/mockup/profile.png"
+  },
+  {
+    "revision": "62ae8a6bada86cdbddb5dd2266ae24df",
+    "url": "/img/mockup/profile_my.png"
+  },
+  {
+    "revision": "18f100e97837e305b1b4cc545e213ae3",
+    "url": "/img/mockup/profile_volk.png"
+  },
+  {
+    "revision": "ef4989acaf4ebdcf1792c96ad785ce55",
+    "url": "/index.html"
+  },
+  {
+    "revision": "916f39a916ce1152c77d",
+    "url": "/js/Announcement.c0b77af0.js"
+  },
+  {
+    "revision": "20ec40598fbd9216e140",
+    "url": "/js/Chats.fff3fec6.js"
+  },
+  {
+    "revision": "b1b8c85a2439566c13f2",
+    "url": "/js/Classrooms.ad6efda2.js"
+  },
+  {
+    "revision": "3692bf771cee8256f613",
+    "url": "/js/Home.106b9f3e.js"
+  },
+  {
+    "revision": "254d03c8fa4ae856057e",
+    "url": "/js/Login.051a713f.js"
+  },
+  {
+    "revision": "ebcdd4b9395afc266a8e",
+    "url": "/js/Regis.3d4d3887.js"
+  },
+  {
+    "revision": "3abe3b5aa79b4d15e8df",
+    "url": "/js/Search.013e3d13.js"
+  },
+  {
+    "revision": "8daa152d5491ffdd7381",
+    "url": "/js/Settings.c9968905.js"
+  },
+  {
+    "revision": "dec554aec515c0b6d026",
+    "url": "/js/Welcome.3ccfde3b.js"
+  },
+  {
+    "revision": "e7e54dbd5070b1567613",
+    "url": "/js/app.74795c8f.js"
+  },
+  {
+    "revision": "e93dab2da991065ca6f8",
+    "url": "/js/chunk-vendors.2e201078.js"
+  },
+  {
+    "revision": "03cbb5cb94661626f6526d0079d66610",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
