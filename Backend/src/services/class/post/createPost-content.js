d035d501068cb86be8adde8a075f1f71a2209151
volktgod <volktgod@gmail.com>
2021-05-27 11:38:28 +0700

    Update createPost-content.js

--- Backend/src/services/class/post/createPost-content.js
+++ Backend/src/services/class/post/createPost-content.js
@@ -9,6 +9,7 @@ createPost.post("/api/createcontent", (req, res) => {
         //REQ POST ID
         id: req.body.id,
         content: req.body.content,
+        announce: req.body.announce,
         update_by: req.body.update_by,
         isShow: req.body.isShow
     }
@@ -24,7 +25,7 @@ createPost.post("/api/createcontent", (req, res) => {
                 console.log(results)
                 if (results.length > 0) {
                     let post_id = results[0].id;
-                    dbCon.query("UPDATE post SET content = ?, isShow = ? WHERE id = ?", [post.content,post.isShow, post_id], (error, results) => {
+                    dbCon.query("UPDATE post SET content = ?, isShow = ?, announce = ?, WHERE id = ?", [post.content,post.isShow,post.announce, post_id], (error, results) => {
                         if (error) throw error;
 
                         return res.status(200).send({
